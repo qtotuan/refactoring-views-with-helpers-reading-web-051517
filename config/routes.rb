282dd3c0a94250c83d8407a9da1733b47d348c7d
qtotuan <quynh.totuan@gmail.com>
2017-06-16 16:37:53 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
   get 'test/index'
 
   resources :posts, only: [:index, :show, :new, :create, :edit, :update]
+  resources :authors
 end
--- spec/controllers/authors_controller_spec.rb
+++ spec/controllers/authors_controller_spec.rb
@@ -3,8 +3,11 @@ require 'rails_helper'
 RSpec.describe AuthorsController, type: :controller do
 
   describe "GET #show" do
+
+
     it "returns http success" do
-      get :show
+      @author = Author.create(name: "Antoine")
+      get :show, id: @author.id
       expect(response).to have_http_status(:success)
     end
   end
